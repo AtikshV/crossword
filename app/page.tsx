78464b631446e323fbf49f2b501485d8b82cda74
Atiksh Vaish <atikshvaish@gmail.com>
2026-03-29 12:20:58 -0400

    Polished navigation, added state between renders

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,11 @@
 "use client"
-import { useState, useRef, useEffect, act } from 'react';
-import Image from "next/image";
+import { useState, useRef, useEffect, act, useActionState } from 'react';
+import puzzle from './test2.json'
 
-// import { parse } from "@xwordly/xword-parser";
-// import { readFileSync } from "fs";
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import { setDefaultAutoSelectFamily } from 'net';
-import { setHeapSnapshotNearHeapLimit } from 'v8';
-import { CellType } from '@xwordly/xword-parser';
-import { IncomingMessage } from 'http';
-import puzzle from './testMini.json'
-import { TestCache } from '@mui/x-data-grid/internals';
-import next from 'next';
-import { Preahvihear } from 'next/font/google';
-// const fileContent = readFileSync('USA Today - 20260109 - One of Two Ways.puz'); 
-// const puzzle = parse(fileContent); 
+
 const cells = puzzle.cells
-
 const cluesAcross = puzzle.clues.across
-
 const cluesDown = puzzle.clues.down
-
-// console.log(puzzle.clues); 
-
-// const columnsAcross: GridColDef[] = [
-//   { field: 'number', headerName: ' ', width: 50 },
-//   { field: 'text', headerName: 'Across', width: 300 },
-// ];
-// const columnsDown: GridColDef[] = [
-//   { field: 'number', headerName: ' ', width: 50 },
-//   { field: 'text', headerName: 'Down', width: 300 },
-// ];
 
 
 
@@ -39,13 +14,24 @@ export default function Home() {
   const inputRefs = useRef<{ [key: string]: HTMLInputElement | null }>({});
   const acrossClueRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
   const downClueRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const lastClickedCellId = useRef<string | null>(null)
 
 
 
   const [activeCell, setActiveCell] = useState<typeof cells[0] | null>(null);
   const [activeOrientation, setActiveOrientation] = useState<"across" | "down">("across") 
-  const [cellValues, setCellValues] = useState<{ [key: string]: string }>({});
 
+  const [cellValues, setCellValues] = useState<{ [key: string]: string }>( () => {
+    if (typeof window === "undefined") return {}
+
+    try { 
+      const saved = localStorage.getItem("cellVals");
+      return saved ? JSON.parse(saved) : {};
+    } catch {
+      return {}   
+    }
+  });
+  //Scroll clues on sidebar whenever activeCell / Orientation changes
   useEffect(() => {
     if (!activeCell) return;
 
@@ -57,6 +43,18 @@ export default function Home() {
     }
   }, [activeCell, activeOrientation]);
 
+  //Focus first cell on reload
+  useEffect( () => {
+  
+    const firstCell = getFirstUnfilledCell(cluesAcross, filledAcrossClues, "across")
+
+    if(!firstCell) getFirstCell(cluesAcross,"across" )
+  }, [])
+
+  //set cellValues localStorage
+  useEffect( () => {
+    localStorage.setItem("cellVals", JSON.stringify(cellValues))
+  }, [cellValues])
 
   const clueStartMap = new Map<string, number>();
   [...puzzle.clues.across, ...puzzle.clues.down].forEach(clue => {
@@ -64,6 +62,95 @@ export default function Home() {
     if (!clueStartMap.has(key)) clueStartMap.set(key, clue.number);
   });
 
+  const filledAcrossClues = new Set(
+    cluesAcross
+      .filter(
+        clue => cells
+        .filter(c => c.acrossClueNum === clue.number)
+        .every(c => !!cellValues[c.id])
+      )
+      .map(clue => clue.number)
+  )
+
+  const filledDownClues = new Set(
+    cluesDown
+      .filter(
+        clue => cells
+        .filter(c => c.downClueNum === clue.number)
+        .every(c => !!cellValues[c.id])
+      )
+      .map(clue => clue.number)
+  )
+
+const getFirstUnfilledCell = (
+  clueSet: typeof cluesAcross, 
+  filledSet: typeof filledAcrossClues, 
+  dir: typeof activeOrientation,
+  fromEdge = false,
+  backward = false
+) => {
+  const currentClueNum = fromEdge 
+    ? (backward ? Infinity : 0) 
+    : (dir == "across" ? activeCell?.acrossClueNum : activeCell?.downClueNum) ?? 0;
+  const clueNumKey = dir == "across" ? "acrossClueNum" : "downClueNum";
+
+  const ordered = backward ? [...clueSet].reverse() : clueSet;
+
+  const match = backward
+    ? (n: number) => n < currentClueNum
+    : (n: number) => n > currentClueNum;
+
+  const nextClue = ordered.find(
+    cl => !filledSet.has(cl.number) && match(cl.number)
+  );
+
+  if (nextClue) {
+    const nextCell = cells.find(
+      c => c[clueNumKey] === nextClue.number && !c.isBlack && !cellValues[c.id]
+    );
+
+    if (nextCell) {
+      setActiveCell(nextCell);
+      setActiveOrientation(dir);
+      inputRefs.current[nextCell.id]?.focus();
+      return true;
+    }
+  }
+  return false;
+};  
+
+const getFirstCell = (
+  clueSet: typeof cluesAcross, 
+  dir: typeof activeOrientation,
+  fromEdge = false,
+  backward = false
+) => {
+  const currentClueNum = fromEdge
+    ? (backward ? Infinity : 0)
+    : (dir == "across" ? activeCell?.acrossClueNum : activeCell?.downClueNum) ?? 0;
+  const clueNumKey = dir == "across" ? "acrossClueNum" : "downClueNum";
+
+  const ordered = backward ? [...clueSet].reverse() : clueSet;
+  const match = backward
+    ? (n: number) => n < currentClueNum
+    : (n: number) => n > currentClueNum;
+
+
+  const nextClue = ordered.find(cl => match(cl.number));
+
+  if (nextClue) {
+    const nextCell = cells.find(
+      c => c[clueNumKey] === nextClue.number && !c.isBlack
+    );
+    if (nextCell) {
+      setActiveCell(nextCell);
+      setActiveOrientation(dir);
+      inputRefs.current[nextCell.id]?.focus();
+      return true;
+    }
+  }
+  return false;
+};  
   
   const divs = cells.map(cell => {
 
@@ -93,13 +180,16 @@ export default function Home() {
               onClick={() => {
                 //TODO: Should not be able to click on black cells! 
                 //TODO: Click on a clue in the list, sets proper cell and clue as Active. 
-                console.log(highlightedIds)
-                if(!cell.isBlack) {
+                if (!cell.isBlack) {
+                  if (cell.id === lastClickedCellId.current) {
+                    setActiveOrientation(prev => prev === "across" ? "down" : "across");
+                  }
+                  lastClickedCellId.current = cell.id;
                   setActiveCell(cell);
                   inputRefs.current[cell.id]?.focus();
-                } else {
-                  
                 }
+
+
               }}
               // onClick={() => {
               //  if(!cell.isBlack) setActiveCellId(cell.id); 
@@ -128,7 +218,7 @@ export default function Home() {
 
             
             </div>
-
+            { !cell.isBlack && ( 
             <input 
 
                 ref={(el) => {
@@ -137,6 +227,7 @@ export default function Home() {
                 maxLength={1}
                 onFocus={(e) => {
                   // e.target.select();
+
                   setActiveCell(cell);
                 }}
                 type='text'
@@ -161,6 +252,7 @@ export default function Home() {
                   
                 }}
             />
+            )}
 
             
             
@@ -222,8 +314,9 @@ export default function Home() {
           textAlign: "right",
           padding: "0 4px 0 4px",
           fontWeight: "bold",
+          color: filledAcrossClues.has(clue.number) ? "#888" : ""
         }}>{clue.number}</span>
-        <div style={{ flex: 1, overflowWrap: "break-word", wordBreak: "break-word" }}>
+        <div style={{ flex: 1, overflowWrap: "break-word", wordBreak: "break-word", color: filledAcrossClues.has(clue.number) ? "#888" : ""}}>
           {clue.clue}
         </div>
     </div>
@@ -274,9 +367,10 @@ export default function Home() {
             textAlign: "right",
             padding: "0 4px 0 4px",
             fontWeight: "bold",
+            color: filledDownClues.has(clue.number) ? "#888" : ""
           }}>{clue.number}</span>
 
-          <div style={{ flex: 1, overflowWrap: "break-word", wordBreak: "break-word" }}>
+          <div style={{ flex: 1, overflowWrap: "break-word", wordBreak: "break-word", color: filledDownClues.has(clue.number) ? "#888" : ""}}>
             {clue.clue}
           </div>
 
@@ -285,7 +379,7 @@ export default function Home() {
 })
 
 
-  const handleKey = (e: React.KeyboardEvent<HTMLInputElement>, cell: any) => {
+  const handleKey = (e: React.KeyboardEvent<HTMLInputElement>, cell: typeof cells[0]) => {
     
     if(e.metaKey || e.ctrlKey) return;
     console.log(e.key)
@@ -309,7 +403,7 @@ export default function Home() {
       console.log(cell.id)
       console.log(cellValues[cell.id])
 
-      if(cellValues[cell.id] != "") {
+      if(cellValues[cell.id]) {
           setCellValues(prev => ({...prev, [cell.id]: ""}))
       } else if(activeOrientation === "across") {
         const prevCell = cells.find(
@@ -331,6 +425,26 @@ export default function Home() {
           moveCell(cell, "up");
         }
       }
+    }
+    if (e.key === "Tab" || e.key === "Enter") {
+      e.preventDefault();
+
+      const otherDir = activeOrientation === "across" ? "down" : "across";
+      const otherClueSet = activeOrientation === "across" ? cluesDown : cluesAcross;
+      const otherFilledSet = activeOrientation === "across" ? filledDownClues : filledAcrossClues;
+      const currentClueSet = activeOrientation === "across" ? cluesAcross : cluesDown;
+      const currentFilledSet = activeOrientation === "across" ? filledAcrossClues : filledDownClues;
+
+      const backward = e.shiftKey; 
+
+
+      getFirstUnfilledCell(currentClueSet, currentFilledSet, activeOrientation,false, backward)        // next unfilled in current direction
+        || getFirstUnfilledCell(otherClueSet, otherFilledSet, otherDir, true, backward)           // next unfilled in other direction (from start)
+        || getFirstUnfilledCell(currentClueSet, currentFilledSet, activeOrientation, true, backward) // wrap around current direction
+        || getFirstCell(currentClueSet, activeOrientation, false, backward)                             // next clue in current direction
+        || getFirstCell(otherClueSet, otherDir, true, backward)                                  // next clue in other direction (from start)
+        || getFirstCell(currentClueSet, activeOrientation, true, backward);                      // wrap around to first clue
+
     }
     if(e.key == "ArrowRight") {
       setActiveOrientation("across");
@@ -356,8 +470,26 @@ export default function Home() {
     switch (dir) {
       case "right":
         nextCell = cells.find(
-          c => c.col == currentCell.col + 1 && !c.isBlack && c.row == currentCell.row
+          c => !c.isBlack && c.row == currentCell.row && !cellValues[c.id] && c != currentCell && c.col >= currentCell.col
+            && c.acrossClueNum == currentCell.acrossClueNum
         );
+
+        if(!nextCell) {
+          console.log("going back")
+          console.log(cellValues)
+          nextCell = cells.find(
+            c => !c.isBlack && c.row == currentCell.row && !cellValues[c.id]
+              && c.col < currentCell.col && c.acrossClueNum == currentCell.acrossClueNum
+          )
+        }
+        if(!nextCell) {
+          nextCell = cells.find(
+            c => !c.isBlack && c.row == currentCell.row && c.acrossClueNum == currentCell.acrossClueNum && c.col == currentCell.col + 1
+          )
+        }
+        console.log("next cell forwarding: ")
+        console.log(nextCell)
+        console.log(cellValues)
         break;
       case "left":
         nextCell = cells.find(
@@ -365,9 +497,31 @@ export default function Home() {
         );
         break;
       case "down":
+        // nextCell = cells.find(
+        //   c => c.col == currentCell.col && c.row == currentCell.row + 1 && !c.isBlack
+        // );
+
         nextCell = cells.find(
-          c => c.col == currentCell.col && c.row == currentCell.row + 1 && !c.isBlack
+          c => !c.isBlack && c.col == currentCell.col && !cellValues[c.id] && c != currentCell && c.row >= currentCell.row
+            && c.downClueNum == currentCell.downClueNum
         );
+
+        if(!nextCell) {
+          console.log("going back")
+          console.log(cellValues)
+          nextCell = cells.find(
+            c => !c.isBlack && c.col == currentCell.col && !cellValues[c.id]
+              && c.row < currentCell.row && c.downClueNum == currentCell.downClueNum
+          )
+        }
+        if(!nextCell) {
+          nextCell = cells.find(
+            c => !c.isBlack && c.col == currentCell.col && c.downClueNum == currentCell.downClueNum && c.row == currentCell.row + 1
+          )
+        }
+        console.log("next cell forwarding: ")
+        console.log(nextCell)
+        console.log(cellValues)
         break;
       case "up":
         nextCell = cells.find(
@@ -380,7 +534,7 @@ export default function Home() {
           c => c.col == currentCell.col - 1 && c.row == currentCell.row
         );
 
-        while(nextCell?.isBlack && currentCell.col >= 0) {
+        while(nextCell?.isBlack && currentCell.col >= 0 && nextCell) {
           nextCell = cells.find(
             c => c.col == nextCell.col - 1 && c.row == currentCell.row
           );
@@ -391,7 +545,7 @@ export default function Home() {
           c => c.col == currentCell.col + 1 && c.row == currentCell.row
         );
 
-        while(nextCell?.isBlack && currentCell.col < puzzle.meta.width) {
+        while(nextCell?.isBlack && currentCell.col < puzzle.meta.width && nextCell) {
           nextCell = cells.find(
             c => c.col == nextCell.col + 1 && c.row == currentCell.row
           );
@@ -402,7 +556,7 @@ export default function Home() {
           c => c.col == currentCell.col && c.row == currentCell.row - 1
         );
 
-        while(nextCell?.isBlack && currentCell.row >= 0)
+        while(nextCell?.isBlack && currentCell.row >= 0 && nextCell)
           nextCell = cells.find(
             c => c.col == currentCell.col && c.row == nextCell.row - 1
           )
@@ -431,8 +585,15 @@ export default function Home() {
     }
   };
 
+  const clueText = activeOrientation == "across"
+    ? cluesAcross.find(cl => cl.number == activeCell?.acrossClueNum)?.clue
+    : cluesDown.find(cl => cl.number == activeCell?.downClueNum)?.clue
+  
+  const clueLabel = activeCell
+    ? (activeOrientation === "across" ? activeCell.acrossClueNum + "A" : activeCell.downClueNum + "D")
+    : null;
 
-
+  const gridHeight = puzzle.meta.height * 50
   return (
     
     <div>
@@ -440,6 +601,24 @@ export default function Home() {
 
         <div style={{display: "flex"}}>
           <div style={{margin: "40px 50px 0px 40px", width: "fit-content"}}>
+            <div style={{
+              backgroundColor: "#deedf6",
+              padding: "16px 20px",
+              marginBottom: "8px",
+              borderRadius: "4px",
+              fontSize: "16px",
+              lineHeight: "1.4",
+              display: "flex",
+              alignItems: "baseline",
+              gap: "12px",
+              minHeight: "52px"
+            }}>
+
+              {clueLabel && (
+                <span style={{ fontWeight: "bold", flexShrink: 0 }}>{clueLabel}</span>
+              )}
+              <span>{clueText}</span>
+            </div>
             <div style={{
               display: "grid", 
               gridTemplateColumns: `repeat(${puzzle.meta.width}, 50px)`, 
@@ -456,17 +635,17 @@ export default function Home() {
             </div>
           </div>
 
-          <div>
+          <div style={{marginTop: "40px"}} >
             <h3 style={{textAlign: "left", paddingLeft: "14px"}}><b>ACROSS</b></h3>
             <hr style={{color: "lightgray"}}></hr>
-            <ol style={{overflowY: "scroll", height: "700px", overflowX: "hidden", width: "300px"}}>
+            <ol style={{overflowY: "scroll", height: `${gridHeight+40}px`, overflowX: "hidden", width: "300px"}}>
               <li>{divsAcross}</li>
             </ol>
           </div>
-          <div style={{marginLeft: "30px"}}>
+          <div style={{marginLeft: "30px", marginTop: "40px"}}>
             <h3 style={{textAlign: "left", paddingLeft: "14px"}}><b>DOWN</b></h3>
             <hr style={{color: "lightgrey"}}></hr>
-            <ol style={{overflowY: "scroll", height: "700px", overflowX: "hidden", width: "300px"}}>
+            <ol style={{overflowY: "scroll", height: `${gridHeight+40}px`, overflowX: "hidden", width: "300px"}}>
               <li>{divsDown}</li>
             </ol>
           </div> 
